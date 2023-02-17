911beaf42ae09f6f79a8416eaa20c10a7fd4d17a
DaySlacker <111376118+AshlynBarrera@users.noreply.github.com>
2023-02-17 09:31:37 -0600

    Created scipt.py file, finished inital question and input program.

---
+++ script.py
@@ -0,0 +1,27 @@
+
+#Variables and User Input
+
+# 2/17/23 Ashlyn Barrera, 4.2 ExerciseB 
+
+
+#My addition-
+# adds interaction to the program, and added \n feature to separate lines in the terminal/console.
+
+print("Welcome to the Age. Im the age bot; here for your assistance")
+#Q3
+name = input('\nPlease enter your name:  ');
+
+#q4 - the value of the name is be what ever the user input it as.
+
+#Q5 
+print("\nHello " + name + ".")
+
+
+#Q7
+age = int(input("\n How old are you? "));
+
+#Q8
+age = age + 10 
+
+#Q9 
+print("\n" +name + "..your gonna be " + str(age) + " in 10 years...woah.") 
